c0c4105b8ca34893f8b0666a1fd2ace90cb77632
AquaticBlue <tokiarb@gmail.com>
2025-12-01 22:46:49 +0000

    test2

--- src/Coach.java
+++ src/Coach.java
@@ -1,7 +1,9 @@
-public class Coach {
+import java.io.File;
 
+public class Coach implements Bruger {
 
     public Coach(){} //Default Constructor
+
 
 
     public void topFem(){}
@@ -9,4 +11,37 @@ public class Coach {
 
     public void opdaterResultat(){}
 
+
+    //Funktion til at få password fra password filen
+    public static String getBrugernavn(){
+
+        File file = new File("kontoer.txt");
+
+        String brugernavn = "";
+        brugernavn = FileUtil.laesDataFraFil(file, "coach", 2);
+
+        return brugernavn;
+    }
+
+
+    //Funktion til at få password fra password filen
+    public static String getPassword(){
+
+        File file = new File("kontoer.txt");
+
+        String password = "";
+        password = FileUtil.laesDataFraFil(file, "coach", 3);
+
+        return password;
+    }
+
+    @Override
+    public void logud() {
+
+    }
+
+    @Override
+    public void menu() {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,51 @@
-public class Main {
-    public static void main(String[] args) {
-        boolean taendt = true;
-       /* while (taendt) {
-            Bruger bruger;
-            login();
-            if (bruger == coach) {
-                Coach.menu();
-            } else if (bruger == Formand) {
-                Formand.menu();
-            } else if (bruger == Kasserer) {
-                Kasserer.menu();
+import java.io.File;
+import java.util.Scanner;
 
+public class Main {
+
+    public static String bruger;
+    public static boolean taendt = true;
+
+    public static void main(String[] args) {
+
+        while (taendt) {
+
+            login();
+
+            if (bruger == "coach") {
+                //Coach.menu();
+            } else if (bruger == "formand") {
+                //Formand.menu();
+            } else if (bruger == "kasserer") {
+                //Kasserer.menu();
+
+            }
+        }
+
+
+        File fil = new File("medlem.txt");
+
+        FileUtil.aendreDataIFil(fil, "Toki", 2, "Bak");
+    }
+
+
+    //Login Funktionen
+    protected static void login() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== LOGIN =============");
+        System.out.println("Brugernavn:");
+        String brugernavn = input.next();
+
+        System.out.println("Brugernavn: " + brugernavn);
+        System.out.println("Password:");
+
+        if (Coach.getBrugernavn().equals(brugernavn)) {
+            String password = input.next();
+
+            if (Coach.getPassword().equals(password)) {
+                bruger = "coach";
+                System.out.println("Du er nu logget ind som coach");
             }
         }
     }

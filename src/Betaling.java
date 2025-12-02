7748fce40b70289aca4fd10896699ed4b3207e3d
MariamGh2 <magh0004@stud.ek.dk>
2025-12-02 13:59:24 +0100

    Tilføjet metoder til bergning af medlemspris

--- src/Betaling.java
+++ src/Betaling.java
@@ -1,7 +1,29 @@
 public class Betaling {
 
 
-    public Betaling(){} //Default Constructor
+    //Attribut der holder styr på om medlem har betalt
+    public Betaling() {
+        this.harBetalt = false;
+    }
 
-    
+    //Gemmer om medlem har betalt
+    private boolean harBetalt;
+
+    public boolean betalKontigent(int alder, boolean aktiv){
+       //Hvis medlem har betalt
+        if (harBetalt) {
+            return true;
+        }
+
+        //Hvis medlem IKKE har betalt
+        Pris prisberegner = new Pris();
+        int pris = prisberegner.beregnPris(alder, aktiv);
+
+        System.out.println("Kontigent pris: " + pris + "kr.");
+
+        harBetalt = true;
+
+        return true;
+    }
 }
+

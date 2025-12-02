f3a51fa7e39e09c73748c24f660aa0830ce57ab2
MariamGh2 <magh0004@stud.ek.dk>
2025-12-02 09:20:47 +0100

    Tilføjet metoder i datoklassen, i forhold til kontigentbetaling

--- src/Dato.java
+++ src/Dato.java
@@ -15,12 +15,35 @@ public class Dato {
         return LocalDate.now().format(Dato);
     }
 
-    // tjekker om datoen er den 1.januar, returnerer en boolean, hvor den viser true, hvis dagens dato er 1.januar og false, hvis det er en hvilken som helst dag
+    //Boolean bliver true hvis dagens dato er d.1 januar og false hvis det ikke er
     public static boolean betalingsDato(){
         LocalDate iDag = LocalDate.now();
 
         return (iDag.getDayOfMonth() == 1 && iDag.getMonthValue() == 1);
     }
+/*
+    // Metode der tjekker om medlem skal have rykker
+    public static boolean skalHaveRykker(LocalDate sidsteBetalingDato) {
+
+    //betalingsdato i nuværende år
+    LocalDate betalingsdato = LocalDate.of(LocalDate.now().getYear(), 1, 1);
+
+    // Betalingsfrist: 1. februar på nuværende år.
+    LocalDate deadline = LocalDate.of(LocalDate.now().getYear(), 1, 2);
+
+    // Hvis medlem aldrig har betalt (dato = null), fx ved nyt medlem
+    if (sidsteBetalingDato == null) {
+        return LocalDate.now().isAfter(deadline);
+    }
+
+    // Hvis sidste betaling var efter betalingsdato og før rykkerfristen, så ingen rykker
+    if (!sidsteBetalingDato.isBefore(deadline)) {
+        return false;
+    }
+
+    // Hvis sidste betaling var før betalingsdato og dagensdato er over rykkerfristen, så rykker
+    return LocalDate.now().isAfter(deadline);
+}*/
 
 }
 

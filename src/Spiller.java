94854db2f36a332c9ebc938b6745b290ca7972d5
Cassandra1801 <cassandramichaelsen@gmail.com>
2025-12-07 20:39:52 +0100

    Main virker - hvert nyt medlem får et nyt nummer i rækken. Der er lavet to constructor til medlem - en for nyoprettet medlemmer og en der skal læses fra fil ift. betaling af kontingent.
    Rettet fejl i FileUtil og formand klassen

--- src/Spiller.java
+++ src/Spiller.java
@@ -7,27 +7,27 @@ public class Spiller extends Medlem {
 
     private File medlemFil = new File("medlem.txt");
 
-    private String type;  // "motionist" eller "konkurrencespiller"
+    private String spillerType;  // "motionist" eller "konkurrencespiller"
 
     // 1) Bruges når du OPRETTER en ny spiller i programmet
     public Spiller(String navn, boolean medlemskab, String foedselsdag,
-                   String mail, String type) {
+                   String mail, String spillerType) {
 
         // Her genererer Medlem SELV medlemsnummer med Medlemsnummer.hentNytMedlemsnummer()
         super("spiller", navn, medlemskab, foedselsdag, mail);
-        this.type = type;
+        this.spillerType = spillerType;
     }
 
     // 2) Bruges hvis du senere vil læse spillere IND FRA FIL (med eksisterende medlemsnummer)
     public Spiller(String navn, int medlemsNummer, boolean medlemskab,
-                   String foedselsdag, String mail, String type, boolean betaling) {
+                   String foedselsdag, String mail, String spillerType, boolean betaling) {
 
         super("spiller", navn, medlemsNummer, medlemskab, foedselsdag, mail, betaling);
-        this.type = type;
+        this.spillerType = spillerType;
     }
 
-    public String getType() {
-        return type;
+    public String getSpillerType() {
+        return spillerType;
     }
 }
 
